d00947e30912767b749468a77e7db6a9c56a1af0
Juan Tabares <juan.ctt@live.com>
2013-02-20 16:39:27 +0900

    correction to assignment 1

--- hw1/part1.rb
+++ hw1/part1.rb
@@ -10,28 +10,3 @@ end
 def count_words(str)
   str.split(' ').length
 end
-
-#the code below this line will test your functions.  You should remove everything below this line prior to submitting your file
-
-
-test_str = "there goes the neighborhood"
-
-if palindrome? test_str
-  puts test_str + " is a palindrome!"
-else
-  puts test_str + " is NOT a palindrome!"
-end
-
-
-test_str = "Madam, I'm Adam"
-
-if palindrome? test_str
-  puts test_str + " is a palindrome!"
-else
-  puts test_str + " is NOT a palindrome!"
-end
-
-test_str = "The rent is due on the first day of the month unless the first day of the month falls on a Saturday or Sunday"
-
-word_count = count_words test_str
-puts word_count
