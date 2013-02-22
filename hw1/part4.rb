10e6fe83edcc12fa7c4c60afee96adf81ab65474
Juan Tabares <juan.ctt@live.com>
2013-02-22 11:56:42 +0900

    Completed fourth assignment

--- hw1/part4.rb
+++ hw1/part4.rb
@@ -1,23 +1,43 @@
 class Dessert
-  def initialize(name, calories)
-    # YOUR CODE HERE
-  end
-  
-  def healthy?
-    # YOUR CODE HERE
-  end
-  
-  def delicious?
-    # YOUR CODE HERE
-  end
+	def initialize(name, calories)
+		raise(ArgumentError, "Argument Name must have at least one character") unless name.gsub(" ","").length > 0
+		raise(ArgumentError, "Calories must be numeric") unless calories.kind_of?(Fixnum)
+		@name = name 
+		@calories = calories
+	end
+
+	attr_reader :name
+	attr_reader :calories
+
+	def healthy?
+		@calories < 200
+	end
+
+	def delicious?
+		true
+	end
+	
 end
 
 class JellyBean < Dessert
+  
   def initialize(name, calories, flavor)
-    # YOUR CODE HERE
+    super(name, calories)
+	raise(ArgumentError, "Argument Name must have at least one character") unless flavor.gsub(" ","").length > 0
+	@flavor = flavor
   end
   
+  attr_reader :flavor
+  
   def delicious?
-    # YOUR CODE HERE
+    true unless @flavor == "black licorice"
+	false
   end
+
 end
+
+# d1 = Dessert.new("Dango",500)
+# d2 = JellyBean.new("JellyBean",250,"black licorice")
+
+# puts "Is #{d1.name} Delicious? #{d1.delicious?}"
+# puts "Is #{d2.name} with flavor '#{d2.flavor}' Delicious? #{d2.delicious?}"
