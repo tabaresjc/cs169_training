319833dba38672b520bfd9e302cdf4966d9d3b36
Juan Tabares <juan.ctt@live.com>
2013-02-22 17:48:22 +0900

    Complete 6th Assignment

--- hw1/part6.rb
+++ hw1/part6.rb
@@ -1,5 +1,5 @@
 class Numeric
-  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019, 'dollar' => 1.000}  
   def method_missing(method_id)
     singular_currency = method_id.to_s.gsub( /s$/, '')
     if @@currencies.has_key?(singular_currency)
@@ -8,12 +8,26 @@ class Numeric
       super
     end
   end
+  
+  def in(currency_id)
+	currency = currency_id.to_s.gsub( /s$/, '')
+	return self unless @@currencies.has_key?(currency)
+	self / @@currencies[currency]
+  end
 end
 
 class String
-  # YOUR CODE HERE
+	def palindrome?
+	  ss = self.downcase.gsub(/[^A-Za-z0-9]/,'')
+	  ss == ss.reverse
+	end
 end
 
 module Enumerable
-  # YOUR CODE HERE
+	def palindrome?
+		return false unless self.is_a?(Array)
+		self.map { |x| x.to_s }.join.palindrome?
+	end
 end
+
+
