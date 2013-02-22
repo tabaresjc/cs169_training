2ec96d0be430c89153a63ea09d323d6d717fb853
Juan Tabares <juan.ctt@live.com>
2013-02-22 18:24:14 +0900

    Complete 5th Assignment

--- hw1/part7.rb
+++ hw1/part7.rb
@@ -21,8 +21,8 @@ class CartesianProduct
   end
 end
 
-c = CartesianProduct.new([:a,:b], [4,5])
-c.each { |elt| puts elt.inspect }
+# c = CartesianProduct.new([:a,:b], [4,5])
+# c.each { |elt| puts elt.inspect }
 
-c = CartesianProduct.new([:a,:b], [])
-c.each { |elt| puts elt.inspect }
+# c = CartesianProduct.new([:a,:b], [])
+# c.each { |elt| puts elt.inspect }
