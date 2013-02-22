16dccf4979b3c3f7793430349a2d676e93e2c3ab
Juan Tabares <juan.ctt@live.com>
2013-02-22 16:49:01 +0900

    Complete 5th Assignment

--- hw1/part5.rb
+++ hw1/part5.rb
@@ -3,6 +3,25 @@ class Class
     attr_name = attr_name.to_s   # make sure it's a string
     attr_reader attr_name        # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
-    class_eval "your code here, use %Q for multiline strings"
+	
+    class_eval %Q{
+	  def #{attr_name}=(new_value)
+		@#{attr_name} = new_value
+		@#{attr_name + "_history"} = [nil] unless @#{attr_name + "_history"}		
+		@#{attr_name + "_history"} << new_value
+	  end
+    }
   end
 end
+
+class Foo
+    attr_accessor_with_history :bar
+end
+
+# f = Foo.new
+# f.bar = 1
+# f.bar = 2
+# f = Foo.new
+# f.bar = 4
+# print f.bar_history 
+
